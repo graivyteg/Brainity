5e52ec14a135950a6ec77dafa35b7e078ef88462
ErrorCommander <kai_412@mail.ru>
2022-04-28 21:28:10 +0300

    correct menu panels

---
+++ Assets/Scripts/GamePanelGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanelGenerator : MonoBehaviour
+{
+    [SerializeField] private List<RectTransform> _placesForGame;
+    [SerializeField] private RectTransform _gamePanel;
+
+    private void Start()
+    {
+        RectTransform gamePanel;
+
+        foreach (var panel in _placesForGame)
+        {
+            gamePanel = Instantiate(_gamePanel, panel);
+            gamePanel.gameObject.SetActive(true);
+		}
+    }
+}
